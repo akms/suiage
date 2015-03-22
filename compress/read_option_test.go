f1be62bd9fc384e27ab7b4a6b18c86b74ede43fe
Akimasa Takeuchi <akimasa.takeuchi@gaiax.com>
2015-03-22 17:53:04 +0900

    add test

--- compress/read_option_test.go
+++ compress/read_option_test.go
@@ -6,44 +6,59 @@ import (
 	"os"
 	"testing"
 	"log"
+	"bufio"
 )
 
 func MakeCopy() {
 	var (
-		b           bytes.Buffer
-		cfileWriter io.WriteCloser
+		buf           bytes.Buffer
+		copy_fileWriter io.WriteCloser
+		err error
+		origin_file,copy_file *os.File
 	)
-	origin, err := os.Open("/etc/suiage.conf")
+	origin_file, err = os.Open("/etc/suiage.conf")
 	if err != nil {
 		log.Fatal("can't open file")
 	}
-	io.Copy(&b, origin)
-	defer origin.Close()
-	cfile, _ := os.Create("/tmp/t_suiage.conf")
-	cfileWriter = cfile
-	defer cfile.Close()
-	cfileWriter.Write(b.Bytes())
-	defer cfileWriter.Close()
+	io.Copy(&buf, origin_file)
+	defer origin_file.Close()
+	copy_file, err = os.Create("/tmp/t_suiage.conf")
+	if err != nil {
+		log.Fatal(err)
+	}
+	copy_fileWriter = copy_file
+	defer copy_file.Close()
+	copy_fileWriter.Write(buf.Bytes())
+	defer copy_fileWriter.Close()
 }
 
 func TestReadOption(t *testing.T) {
 	var (
 		read_strings []string
-		test_strings []string
+		fchecker bool
 	)
-
-
+	
+	MakeCopy()
 	read_strings = ReadOption()
-
+	f ,_ := os.Open("/tmp/t_suiage.conf")
+	defer f.Close()
+	scan := bufio.NewScanner(f)
+	for scan.Scan() {
+		s := scan.Text()
+		fchecker = false
+		for _,r := range read_strings {
+			if s == r {
+				fchecker = true
+			}
+		}
+		if !fchecker {
+			t.Errorf("can't find word %s",s)
+		}
+	}
+	os.Remove("/tmp/t_suiage.conf")
 	workingDir, _ := os.Getwd()
 	if workingDir != "/etc" {
 		t.Errorf("workingdir is not /etc. now workingdir is %s", workingDir)
 	}
 	
-	
-	
-	
-	
-	
-
 }
